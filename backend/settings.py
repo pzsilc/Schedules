1381d5a9ba78c8f834d779ef00f762ba2db36f6f
unknown <user@silcpcXXXX.silcare.local>
2021-08-10 12:32:35 +0200

    Correct static url

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = 'home/pzsilc/Schedules/static/'
+STATIC_ROOT = 'Schedules/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
 
